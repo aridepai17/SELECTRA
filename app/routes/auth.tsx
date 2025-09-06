0e6bdbdad54ff9b80569f6e24f53b609fb49d740
aridepai17 <advaithdepai26@gmail.com>
2025-09-06 20:04:23 +0530

    fix: Fix typo in Welcome and and <main> tag

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -19,11 +19,11 @@ const Auth = () => {
     },[auth.isAuthenticated, next])
 
     return (
-        <main className="bg-=url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+        <main className="bg-url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
             <div className="gradient-border shadow-lg">
                 <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
                     <div className="flex flex-col items-center gap-2 text-center">
-                        <h1>Welcom</h1>
+                        <h1>Welcome</h1>
                         <h2>Log In to Continue Your Job Journey</h2>
                     </div>
                     <div>
